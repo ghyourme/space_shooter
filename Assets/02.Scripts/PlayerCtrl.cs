bf2dddda77aa5cbd2a4e65cf6db0932296937898
변유림 <gg.yourme@gmail.com>
2026-04-30 12:34:35 +0900

    플레이어 캐릭터 컨트롤 스크립트

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -1,14 +1,22 @@
 using UnityEngine;
+using UnityEngine.Android;
 
 public class Player : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     //컴포넌트를 캐시 처리할 변수
     private Transform tr;
+    //Animation 컴포넌트를 저장할 변수
+    private Animation anim;
+    //회전 속도 변수
+    [SerializeField] private float turnSpeed = 100.0f;
     [SerializeField] private float moveSpeed = 5.0f;
     void Start()
     {
         tr = GetComponent<Transform>();
+        anim = GetComponent<Animation>();
+
+        anim.Play("Idle");
     }
 
     // Update is called once per frame
@@ -16,15 +24,42 @@ public class Player : MonoBehaviour
     {
         float h = Input.GetAxis("Horizontal");
         float v = Input.GetAxis("Vertical");
+        float r = Input.GetAxis("Mouse X");
 
-        Debug.Log("h:=" + h);
-        Debug.Log("v:=" + v);
+        Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
+        
+        //Translate (이동 방향 * 속력 * Time.deltaTime)
+        tr.Translate(moveDir * moveSpeed * Time.deltaTime);
 
-        //정규화 벡터를 사용한 코드
-        tr.position += new Vector3(h, 0, v) * Time.deltaTime * moveSpeed;
+        //Vector3.up 축을 기준으로 TurnSpeed 만큼 속도로 회전
+        tr.Rotate(Vector3.up * turnSpeed * Time.deltaTime * r);
 
-        //정규화 벡터를 사용하지 않은 코드
-        //transform.position += new Vector3(h, 0, v) *Time.deltaTime * 5.0f;
+        PlayerAnim(h, v);
 
+    }
+
+    void PlayerAnim(float h, float v)
+    {
+        //키보드 입력값을 기준으로 동작할 애니메이션 수행
+
+        if (v >= 0.1f)
+        {
+            anim.CrossFade("RunF");
+        }
+        else if (v <= -0.1f)
+        {
+            anim.CrossFade("RunB");
+        }
+        else if (v <= -0.1f)
+        {
+            anim.CrossFade("Right");
+        }else if (v <= -0.1f)
+        {
+            anim.CrossFade("Left");
+        }
+        else
+        {
+            anim.CrossFade("Idle");
+        }
     }
 }
